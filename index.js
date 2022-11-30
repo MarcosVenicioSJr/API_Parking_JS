8f0dad57e8df7b56b606d836435d4f2ab716dd35
Marcos Venicio <marcos.mendes@fitbank.com.br>
2022-11-30 13:10:57 -0300

    Add nodemon a aplicação. Criação da rota de add Carros

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const express = require('express');
 const app = express();
 app.listen(3000);
 app.use(express.json());
+
+
+let car = []
+let motorcycle = []
+
+app.get('/addVehicle/car', (req, res) => {
+
+})
